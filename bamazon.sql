f321e12763403c26418df37dfde3e25f1a2cc372
Danzer <dan.alexander616@gmail.com>
2019-03-21 01:31:01 -0600

    fixing sql issues

--- bamazon.sql
+++ bamazon.sql
@@ -1,15 +1,49 @@
+-- Drops the bamazon if it exists currently --
 DROP DATABASE IF EXISTS bamazon;
 
+-- Creates the "bamazon" database --
 CREATE DATABASE bamazon;
 
+-- Makes it so all of the following code will affect bamazon --
 USE bamazon;
 
-CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(35) NOT NULL,
-    author VARCHAR(25) NOT  NULL,
-    department VARCHAR(35) NOT NULL,
-    price FLOAT(10) NOT NULL,
-    quantity FLOAT(10),
-    PRIMARY KEY (id)
+
+-- ======= Products Table ======= --
+-- Creates the table "products" within bamazon --
+CREATE TABLE products
+(
+  -- Column: item_id --
+  item_id INTEGER NOT NULL
+  AUTO_INCREMENT PRIMARY KEY, 
+
+  -- Column: product_name --
+  product_name VARCHAR
+  (42),
+
+  -- Column: department_name --
+  department_name VARCHAR
+  (42),
+
+  -- Column: price --
+  price DOUBLE
+  (10,2),
+
+   -- Column: stock_quantity --
+stock_quantity INTEGER
 );
+
+  -- Seed Items into Database
+INSERT INTO Products (title_name, author_name, department_name, price, stock_quantity)
+VALUES
+("Twilight of the Idols", "Nietzsche, Friedrich", "Philosophy", 8, 10),
+("Revolt Against the Modern World", "Evola, Julius", "Philosophy", 10, 10),
+("History of Madness", "Foucault, Michel", "Philosophy", 10,15),
+("The Wasteland", "Eliot, T.S.", "Poetry", 5,6),
+("Bismark: The Man and the Statesman", "Taylor, A.J.P.", "History", 8, 12),
+("The Origins of the Second World War", "Taylor, A.J.P.", "History", 10, 10),
+("Being and Time", "Heidegger, Martin", "Philosophy", 12, 20),
+("The World of Yesterday", "Zweig, Stefan", "Memoir", 15, 5),
+("Anabasis", "Xenophon, A.", "Greek History", 4, 10),
+("Red Famine", "Applebaum, Anne", "History", 5, 10);
+
+  -- ======= Products Table ======= --
--- customer.js
+++ customer.js
@@ -1,75 +1,114 @@
-//Must require certain NPM packages to run
-require("dotenv").config();
-var password = require("./pass.js");
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// Basic requirements
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+// var keys = require('./keys.js');
 
-//Divider to diplayer cleaner code
-var divider = ("\n\n------------------------\n");
-// var newArr = [];
-
-//Pass read using dotenv
-var pass = password.pass.id;
-
-//Connection to local mysql database
+// create the connection information for the bamazon database
 var connection = mysql.createConnection({
     host: "localhost",
+  
+    // Your port; if not 3306
     port: 3306,
+  
+    // Your username
     user: "root",
-    password: pass,
-    database: "bamazon"
-});
+  
+    // Your password
+    password: "Cursed4312!",
+    database: "Bamazon"
+  });
 
+// connect to mySQL database
 connection.connect(function (err) {
     if (err) throw err;
-
-    console.log("connected as id " + connection.threadId + "\n");  
-    // console.log(newArr);
-
-    startFunc(readInv);
+    // get all products
+    getAllProducts().then(function (result) {
+        // then list them
+        result.forEach(function (item) {
+            console.log('Item ID: ' + item.item_id + ' | Product Name: ' + item.product_name + ' | Price: ' + item.price);
+        });
+        // then ask what the user would like to do
+    }).then(function () {
+        return whatWouldYouLike();
+    });
 });
 
-function startFunc(arr) {
-
-    inquirer
-        .prompt(
-            {
-                name: "action",
-                type: "list",
-                message: "Welcome to the Command Line Book store. \nHere is our current inventory. \nWhat book would you like to buy?",
-                choices: arr
-            }
-        )
-        .then(function (answer) {
-            console.log(answer.action);
+function getAllProducts() {
+    return new Promise(function (resolve, reject) {
+        // query for all items in products table
+        connection.query("SELECT * FROM products", function (err, res) {
+            if (err) reject(err);
+            resolve(res);
         });
-}
-
-function qtyFunc() {
-    inquirer
-        .prompt({
-            name: "quantity",
-            type: "input",
-            message: "How many of product would you like to buy?"
-        })
-        .then(function (answer) {
-
-        });
-}
-
-function readInv() {
-    connection.query("SELECT name, author, department, price, quantity FROM products ORDER BY department, author, name", function (err, res) {
-        if (err) throw err;
-
-        var newArr = [];
-
-        // Log all results of the SELECT statement
-        for (var i = 0; i < res.length; i++) {
-            newArr.push(res[i].name + " | Author: " + res[i].author + " | Genre: " + res[i].department + " | Price: " + res[i].price + " | Quantity: " + res[i].quantity);
-        }
-
-        return newArr;
-        
-        connection.end();
     });
 }
+
+function whatWouldYouLike() {
+    return inquirer.prompt([{
+        name: 'item_id',
+        message: 'What is the Item ID of the product you want to buy?',
+        type: 'input',
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log('\nPlease enter a valid ID.');
+                return false;
+            }
+        }
+    }, {
+        name: 'number_of_units',
+        message: 'How many units would you like to buy?',
+        type: 'input',
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log('\nPlease enter a valid quantity.');
+                return false;
+            }
+        }
+    }]).then(function (answer) {
+        return new Promise(function (resolve, reject) {
+            // query for all items in products table where the item_id is what was chosen
+            connection.query("SELECT * FROM products WHERE item_id=?", answer.item_id, function (err, res) {
+                if (err) reject(err);
+                resolve(res);
+            });
+        }).then(function (result) {
+            // if there aren't enough of the item
+            if (answer.number_of_units > result[0].stock_quantity) {
+                return "I'm sorry, Dave. I'm afraid you can't buy that many.";
+                // if there are enough
+            } else {
+                var object = {};
+                // answer is the users responses to the prompts
+                object.answer = answer;
+                // result is the results of the query
+                object.result = result;
+                return object;
+            }
+        }).catch(function (err) {
+            console.log(err);
+            connection.destroy();
+        }).then(function (object) {
+            // if there was sufficient quantity
+            if (object.answer) {
+                var newQuantity = object.result[0].stock_quantity - object.answer.number_of_units;
+                var product = object.answer.product_id;
+                var totalCost = (object.result[0].price * object.answer.number_of_units).toFixed(2);
+                // query that updates the quantity of the item
+                connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", [newQuantity, product], function (err, res) {
+                    if (err) reject(err);
+                    console.log('Your total cost is $' + totalCost);
+                    // destroy connection
+                    connection.destroy();
+                });
+            } else {
+                console.log(object);
+                // destroy connection
+                connection.destroy();
+            }
+        });
+    });
+};
